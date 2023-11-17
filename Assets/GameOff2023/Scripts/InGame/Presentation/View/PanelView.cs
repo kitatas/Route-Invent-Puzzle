f4c6002ffb9c95b9c85aff4653d6ab86d2da6fff
kitatas <315kitatas10703@gmail.com>
2023-11-17 22:01:27 +0900

    fix: リファクタ

--- Assets/GameOff2023/Scripts/InGame/Presentation/View/PanelView.cs
+++ Assets/GameOff2023/Scripts/InGame/Presentation/View/PanelView.cs
@@ -10,15 +10,16 @@ namespace GameOff2023.InGame.Presentation.View
     public abstract class PanelView : StageObjectView, IDragHandler, IPointerDownHandler, IPointerUpHandler
     {
         private CameraView _cameraView;
-        private Func<GameState, bool> _isState;
         private List<CellView> _cellViews;
+        private Func<GameState, bool> _isState;
         private Vector3 _startPosition;
 
         public void SetUp(Func<GameState, bool> isState, Vector3 position)
         {
             _cameraView = FindObjectOfType<CameraView>();
-            _isState = isState;
             _cellViews = FindObjectsOfType<CellView>().ToList();
+            _isState = isState;
+
             SetPosition(position);
         }
 
@@ -32,11 +33,11 @@ namespace GameOff2023.InGame.Presentation.View
                 SetPosition(position);
 
                 // 最も近い座標のcellの色を変更する
-                var x = Mathf.RoundToInt(currentPosition.x);
-                var y = Mathf.RoundToInt(currentPosition.y);
                 _cellViews.Each(cell =>
                 {
-                    var color = cell.IsEqualPosition(x, y) ? CellConfig.PLACEABLE_COLOR : CellConfig.DEFAULT_COLOR;
+                    var color = cell.IsEqualPosition(currentXToInt, currentYToInt)
+                        ? CellConfig.PLACEABLE_COLOR
+                        : CellConfig.DEFAULT_COLOR;
                     cell.SetColor(color);
                 });
             }
@@ -49,14 +50,12 @@ namespace GameOff2023.InGame.Presentation.View
 
         public virtual void OnPointerUp(PointerEventData eventData)
         {
-            var x = Mathf.RoundToInt(currentPosition.x);
-            var y = Mathf.RoundToInt(currentPosition.y);
-            var cell = _cellViews.Find(cell => cell.IsEqualPosition(x, y));
+            var cell = _cellViews.Find(cell => cell.IsEqualPosition(currentXToInt, currentYToInt));
             if (cell)
             {
                 // 配置可能であれば配置し、cellの色を戻す
                 cell.SetColor(CellConfig.DEFAULT_COLOR);
-                SetPosition(new Vector3(x, y));
+                SetPosition(new Vector3(currentXToInt, currentYToInt));
             }
             else
             {
--- Assets/GameOff2023/Scripts/InGame/Presentation/View/StageObjectView.cs
+++ Assets/GameOff2023/Scripts/InGame/Presentation/View/StageObjectView.cs
@@ -6,6 +6,9 @@ namespace GameOff2023.InGame.Presentation.View
     {
         public Vector3 currentPosition => transform.position;
 
+        public int currentXToInt => Mathf.RoundToInt(currentPosition.x);
+        public int currentYToInt => Mathf.RoundToInt(currentPosition.y);
+
         public void SetPosition(Vector3 position)
         {
             transform.position = position;
