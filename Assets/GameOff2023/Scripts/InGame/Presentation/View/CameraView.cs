02c123c59be96799286a116342082c9409e0f266
kitatas <315kitatas10703@gmail.com>
2023-11-28 01:04:42 +0900

    fix: カメラサイズ

--- Assets/GameOff2023/Scripts/InGame/Presentation/View/CameraView.cs
+++ Assets/GameOff2023/Scripts/InGame/Presentation/View/CameraView.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GameOff2023.InGame.Presentation.View
 {
     public sealed class CameraView : MonoBehaviour
     {
         [SerializeField] private Camera mainCamera = default;
+        [SerializeField] private CanvasScaler canvasScaler = default;
+
+        private void Awake()
+        {
+            var resolution = canvasScaler.referenceResolution;
+            var r = resolution.y / resolution.x;
+            var s = (float)Screen.height / (float)Screen.width;
+            var d = s / r;
+
+            if (d > 1.0f)
+            {
+                mainCamera.orthographicSize *= d;
+            }
+        }
 
         public Vector3 GetWorldPoint(Vector3 cursorPosition)
         {
